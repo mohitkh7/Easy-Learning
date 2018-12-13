35e7e52ae67c3eed113dcb15be2cc36da7af8a38
Shubham Kumar <shubhamkrai123@gmail.com>
2018-12-13 15:13:37 +0530

    updated models.py

--- learn/models.py
+++ learn/models.py
@@ -18,7 +18,8 @@ class Person(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     profile_picture = models.ImageField(
         upload_to="profile_picture/",
-        default="/profile_picture/default_profile_picture.png")
+        default="/profile_picture/default_profile_picture.png",
+    )
     bio = models.TextField(blank=True)
 
     def __str__(self):
@@ -37,18 +38,16 @@ class Category(models.Model):
         return self.title
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = "categories"
 
 
 class Topic(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     # If a person deletes why is it happening ?
-    person = models.ForeignKey('person', on_delete=models.SET('Anonymous'))
+    person = models.ForeignKey("person", on_delete=models.SET("Anonymous"))
     category = models.ForeignKey(
-        'category',
-        on_delete=models.CASCADE,
-        default=1)
+        "category", on_delete=models.CASCADE, default=1)
     image = models.ImageField(upload_to="topic")
     views = models.IntegerField(default=0)
     slug = AutoSlugField(populate_from="title", unique=True, editable=True)
@@ -59,36 +58,36 @@ class Topic(models.Model):
     def __str__(self):
         return self.title
 
-    '''def save(self,*args,**kwargs):
-		self.slug=slugify(self.title)
-		super(Topic,self).save(*args,**kwargs)
-	'''
+    """def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super(Topic,self).save(*args,**kwargs)
+    """
 
     def get_absolute_url(self):
-        return reverse("TopicDetails", args=[self.slug, ])
+        return reverse("TopicDetails", args=[self.slug])
 
 
 class Resource(models.Model):
     LEVELS = (
-        ('beginner', 'Beginner'),
-        ('intermediate', 'Intermediate'),
-        ('advance', 'Advance'),
+        ("beginner", "Beginner"),
+        ("intermediate", "Intermediate"),
+        ("advance", "Advance"),
     )
 
     METHODS = (
-        ('book', 'Book'),
-        ('ebook', 'eBook'),
-        ('video', 'Video'),
-        ('website', 'Website'),
-        ('blog', 'Blog'),
-        ('mooc', 'MOOC'),
-        ('other', 'Other'),
+        ("book", "Book"),
+        ("ebook", "eBook"),
+        ("video", "Video"),
+        ("website", "Website"),
+        ("blog", "Blog"),
+        ("mooc", "MOOC"),
+        ("other", "Other"),
     )
 
     title = models.CharField(max_length=1000)
     description = models.TextField()
-    person = models.ForeignKey('person', on_delete=models.SET('Anonymous'))
-    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+    person = models.ForeignKey("person", on_delete=models.SET("Anonymous"))
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
     url = models.URLField()
     level = models.CharField(max_length=20, choices=LEVELS)
     language = models.CharField(max_length=30, default="English")
@@ -104,23 +103,23 @@ class Resource(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("TopicDetails", args=[self.topic.slug, ])
+        return reverse("TopicDetails", args=[self.topic.slug])
 
 
 class Review(models.Model):
     star = models.IntegerField()
     text = models.TextField()
-    person = models.ForeignKey('person', on_delete=models.SET('Anonymous'))
-    resource = models.ForeignKey('resource', on_delete=models.CASCADE)
+    person = models.ForeignKey("person", on_delete=models.SET("Anonymous"))
+    resource = models.ForeignKey("resource", on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
-        return reverse("TopicDetails", args=[self.resource.topic.slug, ])
+        return reverse("TopicDetails", args=[self.resource.topic.slug])
 
 
 class Bookmark(models.Model):
-    person = models.ForeignKey('person', on_delete=models.SET('Anonymous'))
-    resource = models.ForeignKey('resource', on_delete=models.CASCADE)
+    person = models.ForeignKey("person", on_delete=models.SET("Anonymous"))
+    resource = models.ForeignKey("resource", on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -128,18 +127,15 @@ class Bookmark(models.Model):
 
 
 class Vote(models.Model):
-    OPTION = (
-        (1, "Upvote"),
-        (0, "Undo"),
-        (-1, "DownVote")
-    )
-    person = models.ForeignKey('person', on_delete=models.SET('Anonymous'))
-    resource = models.ForeignKey('resource', on_delete=models.CASCADE)
+    OPTION = ((1, "Upvote"), (0, "Undo"), (-1, "DownVote"))
+    person = models.ForeignKey("person", on_delete=models.SET("Anonymous"))
+    resource = models.ForeignKey("resource", on_delete=models.CASCADE)
     # 1 == Upvote -1 == Downvote
     value = models.IntegerField(default=1, choices=OPTION)
 
     def __str__(self):
         return "%s on %s by %s" % (self.value, self.resource, self.person)
+
 
 # Signals to change in person field when user changes
 
@@ -164,17 +160,18 @@ def save_user_profile(sender, instance, **kwargs):
     p.name = p.name.title()
     p.save()
 
+
 # Signals to change resource score field when vote changes
 
 
-'''
+"""
 Picking all vote column for given resource than adding their value
-'''
+"""
 
 
 @receiver(post_save, sender=Vote)
 def update_score_on_vote(sender, instance, **kwargs):
-    '''
+    """
     all_vote_for_a_resource = Vote.objects.filter(resource=instance.resource)
     print(Vote.objects.filter(resource=instance.resource).aggregate(Sum('value')))
     ans = 0
@@ -183,17 +180,17 @@ def update_score_on_vote(sender, instance, **kwargs):
     instance.resource.score = ans
     instance.resource.save()
 
-    '''
-    sum_of_vote = Vote.objects.filter(
-        resource=instance.resource).aggregate(
-        Sum('value'))
-    instance.resource.score = sum_of_vote['value__sum']
+    """
+    sum_of_vote = Vote.objects.filter(resource=instance.resource).aggregate(
+        Sum("value")
+    )
+    instance.resource.score = sum_of_vote["value__sum"]
     instance.resource.save()
 
 
-'''
+"""
 Book => Author, Amazon Link, Free PDF, Pages, ISBN No., Publisher
 Website => AlexaRank
 Youtube Video => Channel, Views, Upvotes, Downvotes,
 MOOC => Instructor,
-'''
+"""
