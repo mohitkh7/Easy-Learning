c27fa8fd7d8ff31668aefc0b8033c0a0e3c48b22
dewangmodi <32814054+dewangmodi@users.noreply.github.com>
2019-01-02 22:38:00 +0530

    fix issue #3, Upgrade Project to Django 2
    * changed requirements.txt
    
    * changed decorators.py
    
    * urls update
    
    * removing unrequired lines
    
    * changed the wrong url
    
    * small mistake

--- learn/decorators.py
+++ learn/decorators.py
@@ -2,13 +2,16 @@
 This file define custom decorators just like login_required
 '''
 from django.shortcuts import redirect
+from functools import wraps
+
 def strictly_no_login(function):
+	@wraps(function)
 	def wrap(request,*args,**kwargs):
 		#redirecting to home page if already logged in 
 		if request.user.is_authenticated:
 			return redirect("TopicList")
 		else:
 			return function(request,*args,**kwargs)
-	wrap.__doc__ = function.__doc__
-	wrap.__name__ = function.__name__
+#	wrap.__doc__ = function.__doc__
+#	wrap.__name__ = function.__name__
 	return wrap
